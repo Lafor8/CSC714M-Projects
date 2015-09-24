53905b795b25bcecb0bda642b252f1ad0f007117
Lafor8 <jmtw_84@yahoo.com>
2015-09-25 03:50:12 +0800

    Defined TagalogStemmerResult class

--- src/tagalogStemmer/models/TagalogStemmerResult.java
+++ src/tagalogStemmer/models/TagalogStemmerResult.java
@@ -1,5 +1,41 @@
 package tagalogStemmer.models;
 
-public class TagalogStemmerResult {
+import java.util.ArrayList;
 
+public class TagalogStemmerResult {
+	
+	private ArrayList<String> segments;
+	private int rootIndex;
+	private boolean isRootFound;
+	
+	public TagalogStemmerResult(ArrayList<String> segments, int rootIndex){
+		isRootFound = true;
+		this.segments= segments;
+		this.rootIndex = rootIndex;
+	}
+	
+	public TagalogStemmerResult(){
+		isRootFound = false;
+	}
+	
+	public static TagalogStemmerResult getNullResult(){
+		return new TagalogStemmerResult();
+	}
+
+	public String toString() {
+		if(!isRootFound)
+			return "Error: No Root Found";
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < segments.size(); ++i) {
+			if (i != 0)
+				sb.append(" + ");
+			if (i == rootIndex)
+				sb.append("(" + segments.get(i) + ")");
+			else
+				sb.append(segments.get(i));
+		}
+
+		return sb.toString();
+	}
 }
