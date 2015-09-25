491e42d5b22b0314eb7bbc1b6fbbda7be9e4762d
Lafor8 <jmtw_84@yahoo.com>
2015-09-25 21:03:14 +0800

    added Routine 5

--- src/tagalogStemmer/system/TagalogStemmer.java
+++ src/tagalogStemmer/system/TagalogStemmer.java
@@ -25,29 +25,37 @@ public class TagalogStemmer {
 	}
 	
 	public Word stemWord(String word){
-		
-		//TODO: Stem word
 		RuleEngine ruleEngine = new RuleEngine();
 		
+		// INITIALIZATION
 		ruleEngine.initializeWithWord(word);
-		// ROUTINE 1
+		
+		// ROUTINE 1: Hyphen Search
 
-		// ROUTINE 2
-		// ROUTINE 3
+		// ROUTINE 2: Dictionary Search
+		
+		// ROUTINE 3: /-in-/ Removal
 		Routine routine3 = new Routine();
 		routine3.addRule(new ProductionRule(".+in.+", "", ProductionRule.RULE_TYPE_INFIX));
 		
 		ruleEngine.apply(routine3);
 		
-		// ROUTINE 4
+		// ROUTINE 4: Prefix Removal
+		//TODO:
 
-		// ROUTINE 5
+		// ROUTINE 5: /-um-/ Removal
+		Routine routine5 = new Routine();
+		routine5.addRule(new ProductionRule(".+um.+", "", ProductionRule.RULE_TYPE_INFIX));
 
-		// ROUTINE 6
+		ruleEngine.apply(routine5);
+		// ROUTINE 6: Partial Reduplication
+		//TODO:
 
-		// ROUTINE 7
+		// ROUTINE 7: Suffix Removal
+		//TODO:
 
-		// ROUTINE 8
+		// ROUTINE 8: Full Reduplication/Compounding
+		//TODO:
 		
 		return ruleEngine.getResult();
 	}
