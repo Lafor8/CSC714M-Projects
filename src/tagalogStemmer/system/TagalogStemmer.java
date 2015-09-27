84adfe8d3df35e5d1ecbebe78e8dea2a1e7e794c
Alron <alronjan@yahoo.com>
2015-09-27 19:17:23 +0800

    Added prefixes.

--- src/tagalogStemmer/system/TagalogStemmer.java
+++ src/tagalogStemmer/system/TagalogStemmer.java
@@ -52,17 +52,69 @@ public class TagalogStemmer {
 			routine4.addRule(new AffixRemovalRule("mag", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
 
 			// * Added Custom Rules
-			routine4.addRule(new AffixRemovalRule("paka", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("magsipagpaka", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("nagsipagpaka", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("magsipagpa", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("nagsipagpa", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("magsipag", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("nagsipag", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("magkang", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("magpaka", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("magpati", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("nagkang", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("nagpaka", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("nagpati", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("pagpapa", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("pagsasa", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("tagapag", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
 			routine4.addRule(new AffixRemovalRule("ipagka", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
-			routine4.addRule(new AffixRemovalRule("ipag", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+
+			routine4.addRule(new AffixRemovalRule("ipagpa", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("pakiki", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("ipaki", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("magka", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("magpa", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("magsa", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("magsi", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("nagka", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("nagpa", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("nagsa", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("nagsi", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
 			routine4.addRule(new AffixRemovalRule("pagka", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
-			routine4.addRule(new AffixRemovalRule("pag", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
-			routine4.addRule(new AffixRemovalRule("nag", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
-			routine4.addRule(new AffixRemovalRule("sipag", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("ipag", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			// routine4.addRule(new AffixRemovalRule("mala", "",
+			// AffixRemovalRule.RULE_TYPE_PREFIX, true));
 			routine4.addRule(new AffixRemovalRule("mang", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
-			routine4.addRule(new AffixRemovalRule("in", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
-			routine4.addRule(new AffixRemovalRule("ma", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+
+			routine4.addRule(new AffixRemovalRule("naka", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("paka", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("paki", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			// routine4.addRule(new AffixRemovalRule("pasa", "",
+			// AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("taga", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+
+			routine4.addRule(new AffixRemovalRule("ika", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("ipa", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("mag", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			// routine4.addRule(new AffixRemovalRule("mam", "",
+			// AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			// routine4.addRule(new AffixRemovalRule("man", "",
+			// AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("nag", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("pag", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("pala", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			// routine4.addRule(new AffixRemovalRule("pam", "",
+			// AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			// routine4.addRule(new AffixRemovalRule("tag", "",
+			// AffixRemovalRule.RULE_TYPE_PREFIX, true));
 			routine4.addRule(new AffixRemovalRule("ka", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("ma", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("pa", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			// routine4.addRule(new AffixRemovalRule("i", "",
+			// AffixRemovalRule.RULE_TYPE_PREFIX, true));
+
+			routine4.addRule(new AffixRemovalRule("sipag", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
+			routine4.addRule(new AffixRemovalRule("in", "", AffixRemovalRule.RULE_TYPE_PREFIX, true));
 
 			ruleEngine.apply(routine4);
 		}
