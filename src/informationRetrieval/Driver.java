734fe7ac2c3edf737be7e30352a681e62df8737b
Alron <alronjan@yahoo.com>
2015-11-06 02:29:10 +0800

    Added method stubs for searching the indices.

--- src/informationRetrieval/Driver.java
+++ src/informationRetrieval/Driver.java
@@ -3,12 +3,20 @@ package informationRetrieval;
 import informationRetrieval.index.IDFExtender;
 import informationRetrieval.index.InvertedIndexer;
 import informationRetrieval.index.TFExtender;
+import informationRetrieval.models.Document;
 import informationRetrieval.models.DocumentManager;
 import informationRetrieval.models.InvertedIndex;
 import informationRetrieval.normalization.LowerCaseNormalizer;
 import informationRetrieval.normalization.StopWordRemover;
 import informationRetrieval.normalization.Trimmer;
+import informationRetrieval.search.BasicSearch;
+import informationRetrieval.search.SearchStrategy;
+import informationRetrieval.search.TFIDFSearch;
+import informationRetrieval.search.TFSearch;
 import informationRetrieval.tokenization.RegexTokenizer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Driver {
 
@@ -24,18 +32,40 @@ public class Driver {
 		dm.normalize(new Trimmer());
 		dm.normalize(new LowerCaseNormalizer());
 		dm.normalize(new StopWordRemover());
+		// TODO: implement stop word remover
 		// feel free to add new normalization modules (like expanding
 		// contractions e.g. aso't pusa -> aso at pusa))
 		// look at the normalization package. implement the normalizer
 		// interface then call dm.normalize() here with the new module.
 
+		/* Inverted Index Generation */
+
+		// the extender classes clone the index passed to it for extension, so
+		// there are three different indices: basic, tf, and tfidf
 		InvertedIndex basicIndex = InvertedIndexer.index(dm.getDocumentsAsList());
-		// System.out.println(basicIndex);
-
 		InvertedIndex tfIndex = TFExtender.extend(basicIndex);
-
 		InvertedIndex tfIdfIndex = IDFExtender.extend(tfIndex);
+
 		System.out.println(tfIdfIndex);
 
+		/* Get input from the user */
+
+		// TODO: ask for search string from user.
+		// once the search string is acquired from the user,
+		// tokenize and normalize into a List<String> object
+		List<String> searchTerms = new ArrayList<String>();
+
+		/* Perform the actual search */
+
+		// TODO: implement these three search strategies
+		SearchStrategy basic = new BasicSearch();
+		SearchStrategy tf = new TFSearch();
+		SearchStrategy tfIdf = new TFIDFSearch();
+
+		List<Document> basicResults = basic.search(basicIndex, searchTerms);
+		List<Document> tfResults = tf.search(basicIndex, searchTerms);
+		List<Document> tfIdfResults = tfIdf.search(basicIndex, searchTerms);
+
+		/* Display output based on results */
 	}
 }
---
+++ src/informationRetrieval/search/TFIDFSearch.java
@@ -0,0 +1,16 @@
+package informationRetrieval.search;
+
+import informationRetrieval.models.Document;
+import informationRetrieval.models.InvertedIndex;
+
+import java.util.List;
+
+public class TFIDFSearch implements SearchStrategy {
+
+	@Override
+	public List<Document> search(InvertedIndex index, List<String> searchTerms) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
